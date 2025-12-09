2b23fb3f0eae57bc0b53526685b0dbe17c14b408
HebaElgohary <hebasamir535@gmail.com>
2025-12-09 15:59:33 +0200

    Remove secret

--- frontEnd/Medifind_UI/src/components/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export const ProtectedRoute = ({ isAuthenticated }) => {
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
